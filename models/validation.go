7646b3f347eeeac4b44525f96b52d52aa8b33a6f
johnervine@hotmail.com <johnervine@hotmail.com>
2016-11-06 11:31:30 +0000

    Basic error handling added around creation of user

--- models/validation.go
+++ models/validation.go
@@ -10,12 +10,11 @@ type InputValidation interface {
 }
 
 func DecodeAndValidate(r *http.Request, v InputValidation) error {
-	// json decode the payload - obviously this could be abstracted
-	// to handle many content types
+
 	if err := json.NewDecoder(r.Body).Decode(v); err != nil {
 		return err
 	}
+
 	defer r.Body.Close()
-	// peform validation on the InputValidation implementation
 	return v.Validate(r)
 }
